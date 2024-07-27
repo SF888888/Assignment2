ed7ccc61799ea63b0b98668d3d58df486a1afbb6
SF888888 <fang.su@northeastern.edu>
2024-07-26 17:47:10 -0700

    created Style for toggling

---
+++ contexts/Styles.js
@@ -0,0 +1,21 @@
+export const colors = {
+    light: {
+      background: 'white',
+      text: 'black',
+    },
+    dark: {
+      background: 'black',
+      text: 'white',
+    },
+  };
+  
+  export const globalStyles = {
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+    text: {
+      fontSize: 18,
+    },
+  };
+  
--- contexts/ThemeContext.js
+++ contexts/ThemeContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { colors } from '../helpers/styles';
+import { colors } from './Styles';
 
 const ThemeContext = createContext();
 
