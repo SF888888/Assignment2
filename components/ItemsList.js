6060c1747a01f1faa70b28489ef5fce02114488f
SF888888 <fang.su@northeastern.edu>
2024-07-27 11:46:13 -0700

    tried to solve the undefined create context issue

--- components/ItemsList.js
+++ components/ItemsList.js
@@ -1,4 +1,3 @@
-// ItemsList.js
 import React from 'react';
 import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
